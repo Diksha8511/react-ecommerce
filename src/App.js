72fb894988de9f92f14c451266b9c8ed9a9c3e79
Diksha8511 <diksha85119@gmail.com>
2023-07-02 15:28:02 +0530

    Adding shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ShopPage from "./pages/shop/ShopPage";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
+          <Route path="/shop" element={<ShopPage />}></Route>
         </Routes>
       </div>
     </Router>
